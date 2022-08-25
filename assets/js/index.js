516da57ec3fafe8cc5a7c975b042bda7d129cc42
Reeyna <reeynarl@gmail.com>
2022-08-25 00:59:35 -0500

    Add: functionality for search bar and show meals

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
-import {searchByCategory} from './utils.js'
+import {searchByCategory, searchByName} from './utils.js'
 
+searchByName()
 searchByCategory("recetasPollo", "Chicken", "Recetas con pollo")
 searchByCategory("recetasCarne", "Beef", "Recetas con carne")
 searchByCategory("recetasPescado", "Seafood", "Recetas con mariscos")
